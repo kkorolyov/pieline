38a0effad1bab35d31bae7543073458ad699d36b
kkorolyov <kirillk753@gmail.com>
2020-03-11 19:45:34 -0700

    Init web stuff

---
+++ pie-web/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Project from "../project/Project";
+
+function App() {
+  return (
+    <h1>
+      This is App
+      <Project />
+    </h1>
+  );
+}
+
+export default App;
---
+++ pie-web/src/project/Project.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Project.module.scss";
+
+function Project() {
+  return <h1 className={styles.title}>This be a project</h1>;
+}
+
+export default Project;
