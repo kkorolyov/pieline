f0911c0b98cf05f54cc8420f93a0beac745fd6e8
kkorolyov <kirillk753@gmail.com>
2020-06-30 12:23:58 -0700

    #10: Finalize pi18n service

--- pi18n/src/server.ts
+++ pi18n/src/server.ts
@@ -1,23 +1,6 @@
-import {
-  sendUnaryData,
-  Server,
-  ServerCredentials,
-  ServerUnaryCall,
-} from "grpc";
-import { i18nService, Ii18nServer } from "./proto/i18n_grpc_pb";
-import { i18nPack, PackRequest } from "./proto/i18n_pb";
-import en_us from "./resources/en_us.json";
-
-class i18nServer implements Ii18nServer {
-  get(call: ServerUnaryCall<PackRequest>, callback: sendUnaryData<i18nPack>) {
-    const result = new i18nPack();
-
-    for (const [key, val] of Object.entries(en_us)) {
-      result.getValueMap()[key] = val;
-    }
-    callback(null, result);
-  }
-}
+import { Server, ServerCredentials } from "grpc";
+import { i18nService } from "./generated/proto/internationalization_grpc_pb";
+import i18nServer from "./service/i18nServer";
 
 const server = new Server();
 server.addService(i18nService, new i18nServer());
