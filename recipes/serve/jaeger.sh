db278fdec059cdc009943e4c23aa80e19a790367
kkorolyov <kirillk753@gmail.com>
2020-08-15 01:17:38 -0700

    #15: Add jaeger all-in-one sh

---
+++ recipes/serve/jaeger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+podman run -d --pod pieline --name jaeger \
+	-p 6831:6831/udp \
+	-p 6832:6832/udp \
+	-p 5778:5778 \
+	-p 16686:16686 \
+	-p 14268:14268 \
+	-p 14250:14250 \
+	jaegertracing/all-in-one:1.18
