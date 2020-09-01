7bf59973d62d69f4c782d9423b614f275ad2c82e
kkorolyov <kirillk753@gmail.com>
2020-09-01 01:51:14 -0700

    #8: Add more Waitable

--- pie-web/src/component/user/Profile.tsx
+++ pie-web/src/component/user/Profile.tsx
@@ -1,4 +1,5 @@
-import { Button, CircularProgress, Grid } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
+import Waitable from "component/common/wrapper/Waitable";
 import { ApiContext } from "context";
 import { User_Details } from "generated/graphql";
 import { useArgs, useExecutor, useResult } from "hooks";
@@ -36,37 +37,33 @@ const Profile = ({ id }: ProfileProps) => {
 
   return (
     <Grid container direction="column" spacing={2}>
-      {getExecutor.waiting ? (
-        <CircularProgress />
-      ) : (
-        <>
-          <Grid item>
-            <Display displayName={displayName} email={email} />
-          </Grid>
+      <Waitable waiting={getExecutor.waiting}>
+        <Grid item>
+          <Display displayName={displayName} email={email} />
+        </Grid>
 
-          {!isEditing ? (
-            <Grid item xs={1}>
-              <Button
-                variant="outlined"
-                onClick={() => {
-                  setEditing(true);
-                }}
-              >
-                Edit
-              </Button>
-            </Grid>
-          ) : (
-            <Grid item>
-              <Editor
-                displayName={displayName}
-                email={email}
-                onSubmit={saveExecutor.execute}
-                waiting={saveExecutor.waiting}
-              />
-            </Grid>
-          )}
-        </>
-      )}
+        {!isEditing ? (
+          <Grid item xs={1}>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                setEditing(true);
+              }}
+            >
+              Edit
+            </Button>
+          </Grid>
+        ) : (
+          <Grid item>
+            <Editor
+              displayName={displayName}
+              email={email}
+              onSubmit={saveExecutor.execute}
+              waiting={saveExecutor.waiting}
+            />
+          </Grid>
+        )}
+      </Waitable>
     </Grid>
   );
 };
