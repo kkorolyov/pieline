b4ee6473eb7c6921954b90e2b4bf3390c320ca4d
kkorolyov <kirillk753@gmail.com>
2020-09-20 18:41:44 -0700

    Try bake on ubuntu

--- recipes/bake/common/install.sh
+++ recipes/bake/common/install.sh
@@ -9,6 +9,12 @@ if [ $# -gt 0 ]; then
 
 	if [ -x "$(command -v dnf)" ]; then
 		install="dnf install -y --setopt install_weak_deps=false --setopt cachedir=/var/cache/dnf --installroot $mnt --releasever $releasever"
+	elif [ -x "$(command -v apt)" ]; then
+		install() {
+			apt clean
+			apt install -dy "$@"
+			dpkg -i /var/cache/apt/archives/*.deb --root=$mnt
+		}
 	elif [ -x "$(command -v yay)" ]; then
 		# TODO Explore this more
 		install="yay --root $mnt -S"
