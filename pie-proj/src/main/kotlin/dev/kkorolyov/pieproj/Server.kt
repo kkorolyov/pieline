ced422ce76969f5c91bb55d7b43f00d69231043d
kkorolyov <kirillk753@gmail.com>
2020-12-10 23:55:29 -0800

    #13: Add projects service harness

---
+++ pie-proj/src/main/kotlin/dev/kkorolyov/pieproj/Server.kt
@@ -0,0 +1,29 @@
+package dev.kkorolyov.pieproj
+
+import dev.kkorolyov.pieline.token.TOKEN_INTERCEPTOR
+import dev.kkorolyov.pieproj.trace.SERVER_TRACER
+import io.grpc.ServerBuilder
+import org.slf4j.LoggerFactory
+
+private val PORT = Integer.parseInt(System.getenv("PORT"))
+
+private val LOG = LoggerFactory.getLogger("main").apply {
+	Thread.setDefaultUncaughtExceptionHandler { _, e ->
+		error("unhandled exception", e)
+	}
+}
+
+/**
+ * Starts pie-proj server.
+ */
+fun main() {
+	val server = ServerBuilder.forPort(PORT)
+		.intercept(SERVER_TRACER)
+		.intercept(TOKEN_INTERCEPTOR)
+		.build()
+		.start()
+
+	LOG.info("Started on port{{}}", PORT)
+
+	server.awaitTermination()
+}
---
+++ pie-proj/src/main/kotlin/dev/kkorolyov/pieproj/trace/Tracing.kt
@@ -0,0 +1,34 @@
+package dev.kkorolyov.pieproj.trace
+
+import io.grpc.ClientInterceptor
+import io.grpc.ServerInterceptor
+import io.jaegertracing.Configuration
+import io.opentracing.Tracer
+import io.opentracing.contrib.grpc.TracingClientInterceptor
+import io.opentracing.contrib.grpc.TracingServerInterceptor
+import io.opentracing.util.GlobalTracer
+
+/**
+ * Application tracer.
+ */
+val TRACER: Tracer = Configuration.fromEnv("pie-proj").tracer.also {
+	GlobalTracer.registerIfAbsent(it)
+}
+
+/**
+ * Application gRPC client interceptor.
+ */
+val CLIENT_TRACER: ClientInterceptor = TracingClientInterceptor.newBuilder()
+	.withTracer(TRACER)
+	.withStreaming()
+	.withVerbosity()
+	.build()
+
+/**
+ * Application gRPC server interceptor.
+ */
+val SERVER_TRACER: ServerInterceptor = TracingServerInterceptor.newBuilder()
+	.withTracer(TRACER)
+	.withStreaming()
+	.withVerbosity()
+	.build()
