3a4bf51ccb99b39500c66da2098c45e09702db6c
kkorolyov <kirillk753@gmail.com>
2021-01-01 15:05:08 -0800

    Avoid multiple install passes on bake

--- bake/common/install.sh
+++ bake/common/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
-# Buildah package manager install comman wrapper
+# Buildah package manager install common wrapper
 # Can be used in a buildah unshare environment
 # Packages are installed using 'dnf'
 # Usage: install.sh <container> [package...] 
 
-dnfOpts="-y --setopt install_weak_deps=false --setopt tsflags=nodocs --releasever 32"
+dnfOpts="-y --setopt cachedir=/var/cache/dnf --setopt reposdir=/etc/yum.repos.d --setopt install_weak_deps=false --setopt tsflags=nodocs --releasever 32"
 container="$1"
 shift
 
@@ -14,14 +14,15 @@ if [ "$#" -gt 0 ]; then
 
 	# Try to use the host to install
 	if [ -x "$(command -v dnf)" ]; then
-		dnf install $dnfOpts --setopt cachedir=/var/cache/dnf --setopt reposdir=/etc/yum.repos.d --installroot $mnt $@
+		# avoid locale bloat by forcing minimal glibc
+		dnf install $dnfOpts --installroot $mnt glibc-minimal-langpack $@
 	else
 		# If host is not fedora, make your own fedora
 		echo "host does not have dnf, making my own dnf..."
 		podman run --rm -v $mnt:/mnt registry.fedoraproject.org/fedora bash -c "
 	curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo &&
 	curl -sL https://rpm.nodesource.com/setup_14.x | bash - &&
-	dnf install $dnfOpts --setopt cachedir=/var/cache/dnf --setopt reposdir=/etc/yum.repos.d --installroot /mnt $@
+	dnf install $dnfOpts --installroot /mnt glibc-minimal-langpack $@
 	"
 	fi
 
--- bake/common/prep.sh
+++ bake/common/prep.sh
@@ -63,5 +63,3 @@ fi
 
 # common prep
 container=$(buildah from scratch)
-# avoid locale bloat
-install glibc-minimal-langpack
