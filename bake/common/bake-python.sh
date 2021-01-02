35954190f882acbb98246339687980dae2cdd651
kkorolyov <kirillk753@gmail.com>
2021-01-01 18:40:40 -0800

    Cave to cd in bake-python

--- bake/common/bake-python.sh
+++ bake/common/bake-python.sh
@@ -9,11 +9,13 @@ install python-pip gcc
 swd=${cwd}/../../${service}
 
 # build
+cd $swd
 python -m pip install --user wheel -r requirements.txt
-rm -rf ${swd}/dist
-${swd}/protoc.sh
-python ${swd}/setup.py bdist_wheel
-copy ${swd}/dist dist
+rm -rf dist
+./protoc.sh
+python setup.py bdist_wheel
+copy dist dist
+cd -
 run bash -c "python3 -m pip install --user dist/*"
 run rm -rf dist
 
